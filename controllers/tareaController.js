405349d6dbdf048e2643eb94809c6d295a429f42
jesus cruz <jesus.cruz@campusvirtual.aunarvillavicencio.edu.co>
2022-05-03 19:38:25 -0500

    Modelo, Routing y Funciones para las tareas agregadas

---
+++ controllers/tareaController.js
@@ -0,0 +1,11 @@
+const agregarTarea = async (req, res) => {};
+
+const obtenerTarea = async (req, res) => {};
+
+const actualizarTarea = async (req, res) => {};
+
+const eliminarTarea = async (req, res) => {};
+
+const cambiarEstadoTarea = async (req, res) => {};
+
+export { agregarTarea, obtenerTarea, actualizarTarea, eliminarTarea, cambiarEstadoTarea };
